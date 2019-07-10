c90ddb7593a1e38ac9aaaf44bbc1fb51d9b27ea2
chataolauj <chataolauj@gmail.com>
2019-07-10 19:35:12 -0400

    Fixed env variables

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 const proxy = 'https://cors-anywhere.herokuapp.com/';
-const EVENTFUL_API_KEY = process.env.eventful_api_key || process.env.VUE_APP_EVENTFUL_KEY;
+const EVENTFUL_API_KEY = process.env.VUE_APP_EVENTFUL_KEY;
 const API_URL = `${proxy}http://api.eventful.com/json/events/search?app_key=${EVENTFUL_API_KEY}`;
 
 function getEvents(search_parameters) {
-    console.log(process.env.VUE_APP_EVENTFUL_KEY + " " + process.env.NODE_ENV + " " + process.env.VUE_APP_EVENTFUL_KEY)
     var location = search_parameters.location;
     var date = search_parameters.date;
     var within = search_parameters.within;
