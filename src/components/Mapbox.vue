c808180daf6a672688c41e0eea6218a23789c457
chataolauj <chataolauj@gmail.com>
2021-03-05 12:48:56 -0500

    Created Mapbox vue component file.

---
+++ src/components/Mapbox.vue
@@ -0,0 +1,15 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	name: "Mapbox",
+	data() {
+		return {};
+	},
+	mounted() {},
+};
+</script>
+
+<style lang="scss" scoped></style>
